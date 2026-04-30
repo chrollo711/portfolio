4c2c47f9d806ec83ee521b0d374207d34a231ee5
aetheris-ai-serv <dataisservixe@gmail.com>
2026-04-30 22:11:00 +0530

    first commit

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
 import Hero from '../components/Hero'
 import About from '../components/About'
-import Skills from '../components/Skills'
-import Projects from '../components/Projects'
-import Services from '../components/Services'
+import {Skills} from '../components/Skills'
+import {Projects} from '../components/Projects'
+import {Services} from '../components/Services'
 import Contact from '../components/Contact'
 
 function HomePage() {
--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,4 +1,4 @@
-import Projects from '../components/Projects.tsx'
+import {Projects} from '../components/Projects.tsx'
 
 function ProjectsPage() {
   return (
